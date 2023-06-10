5aa06ef8de7f00e6d9f045fec73fe7aba72d5cbe
Mukan Singh <mukeshschouhan111@gmail.com>
2023-06-10 12:16:06 +0530

    commited

---
+++ js/script.js
@@ -0,0 +1,158 @@
+// Swiper Slider
+        var swiper = new Swiper(".mySwiperslide", {
+            spaceBetween: 30,
+            effect: "fade",
+            speed: 1000,
+            loop: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+            },
+        });
+        var swiper = new Swiper(".mySwiper", {
+            direction: "vertical",
+            slidesPerView: 1,
+            spaceBetween: 30,
+            speed: 1000,
+            loop: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+            },
+        });
+
+
+
+// GSAP
+
+        gsap.to(".hero-scroll-images-wrapper", {
+            xPercent: 700,
+            x: () => innerWidth,
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".hero-scroll-images-wrapper",
+                start: "top bottom",
+                end: () => innerWidth * 18,
+                scrub: true,
+                // pin: true,
+                invalidateOnRefresh: true,
+                anticipatePin: 1,
+                // markers: true,
+            },
+
+        });
+        
+
+        
+
+
+
+        ScrollTrigger.matchMedia({
+            '(min-width:768px)':function(){
+              
+                gsap.to(".hero-scroll-image", {
+                    ease: "none",
+                    rotation: 360,
+                    scrollTrigger: {
+                        trigger: ".hero-scroll-images-wrapper",
+                        start: "top bottom",
+                        end: () => innerWidth * 7,
+                        scrub: true,
+                        // pin: true,
+                        invalidateOnRefresh: true,
+                        anticipatePin: 1,
+                        // markers: true,
+                    },
+        
+                });
+                gsap.to(".scroll-para", {
+                    xPercent: 100,
+                    x: () => innerWidth,
+                    ease: "none",
+                    scrollTrigger: {
+                        trigger: ".scroll-para",
+                        start: "top bottom",
+                        end: () => innerWidth * 10,
+                        scrub: true,
+                        // pin: true,
+                        invalidateOnRefresh: true,
+                        anticipatePin: 1
+                    }
+                });
+                gsap.to(".scroll-para-last", {
+                    xPercent: -100,
+                    x: () => innerWidth,
+                    ease: "none",
+                    scrollTrigger: {
+                        trigger: ".scroll-para-last",
+                        start: "top bottom",
+                        end: () => innerWidth * 10,
+                        scrub: true,
+                        // pin: true,
+                        invalidateOnRefresh: true,
+                        anticipatePin: 1
+                    }
+                });
+              
+            },
+            '(max-width:767px)':function(){
+              
+                gsap.to(".hero-scroll-image", {
+                    ease: "none",
+                    rotation: 360,
+                    scrollTrigger: {
+                        trigger: ".hero-scroll-images-wrapper",
+                        start: "top center",
+                        end: () => innerWidth * 15,
+                        scrub: true,
+                        // pin: true,
+                        invalidateOnRefresh: true,
+                        anticipatePin: 1,
+                        // markers: true,
+                    },
+        
+                });
+                gsap.to(".scroll-para", {
+                    xPercent: 100,
+                    x: () => innerWidth,
+                    ease: "none",
+                    scrollTrigger: {
+                        trigger: ".scroll-para",
+                        start: "top bottom",
+                        end: () => innerWidth * 15,
+                        scrub: true,
+                        // pin: true,
+                        invalidateOnRefresh: true,
+                        anticipatePin: 1
+                    }
+                });
+                gsap.to(".scroll-para-last", {
+                    xPercent: -100,
+                    x: () => innerWidth,
+                    ease: "none",
+                    scrollTrigger: {
+                        trigger: ".scroll-para-last",
+                        start: "top bottom",
+                        end: () => innerWidth * 15,
+                        scrub: true,
+                        // pin: true,
+                        invalidateOnRefresh: true,
+                        anticipatePin: 1
+                    }
+                });
+              
+            }
+          })
+          
+
+
+
+
+
+        
